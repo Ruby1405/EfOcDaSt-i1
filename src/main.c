a0c0cfc06736fccd259f6cbcf4629225f22519eb
Ruby <ruby.calmerman@gmail.com>
2025-11-24 20:50:49 +0100

    bouncing balls

--- src/main.c
+++ src/main.c
@@ -8,45 +8,100 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+#define uint16 unsigned short
+#define uint32 unsigned int
+#define sint16 short
+#define sint32 int
 
 int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	const uint16 BOARD_WIDTH = 1980;
+	const uint16 BOARD_HEIGHT = 1020;
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	InitWindow(BOARD_WIDTH, BOARD_HEIGHT, "Voronoi");
+	ToggleFullscreen();
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
+	// // Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	// SearchAndSetResourceDir("resources");
+
+	// // Load a texture from the resources directory
+	// Texture wabbit = LoadTexture("wabbit_alpha.png");
+
+	// ----------- my stuff ------------
+	const sint16 SEED_COUNT = 20;
+	Vector2 seeds[SEED_COUNT];
+	for (sint16 i = 0; i < SEED_COUNT; i++)
 	{
-		// drawing
+		seeds[i].x = (float)(GetRandomValue(0, 1980));
+		seeds[i].y = (float)(GetRandomValue(0, 1020));
+	}
+	Vector2 seedVels[SEED_COUNT];
+	for (sint16 i = 0; i < SEED_COUNT; i++)
+	{
+		seedVels[i].x = (float)(GetRandomValue(-2, 2));
+		seedVels[i].y = (float)(GetRandomValue(-2, 2));
+	}
+	
+	while (!WindowShouldClose())
+	{	
 		BeginDrawing();
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		for (sint16 i = 0; i < SEED_COUNT; i++)
+		{
+			DrawCircleV(seeds[i], 2.0, WHITE);
+		}
 		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		
 		EndDrawing();
+
+		// Move seeds and bounce off walls
+		for (sint16 i = 0; i < SEED_COUNT; i++)
+		{
+			seeds[i] = Vector2Add(seeds[i], seedVels[i]);
+
+			seedVels[i].x = (
+				(-seedVels[i].x * (0 > seeds[i].x || BOARD_WIDTH <= seeds[i].x)) +
+				(seedVels[i].x  * (0 <= seeds[i].x && BOARD_WIDTH > seeds[i].x))
+			);
+
+			seedVels[i].y = (
+				(-seedVels[i].y * (0 > seeds[i].y || BOARD_HEIGHT <= seeds[i].y)) +
+				(seedVels[i].y  * (0 <= seeds[i].y && BOARD_HEIGHT > seeds[i].y))
+			);
+
+			seeds[i].x = (
+				(-seeds[i].x * (0 > seeds[i].x)) +
+				(seeds[i].x  * (0 <= seeds[i].x))
+			);
+
+			seeds[i].y = (
+				(-seeds[i].y * (0 > seeds[i].y)) +
+				(seeds[i].y  * (0 <= seeds[i].y))
+			);
+
+			seeds[i].x = (
+				((BOARD_WIDTH - (seeds[i].x - BOARD_WIDTH)) * (BOARD_WIDTH <= seeds[i].x)) +
+				(seeds[i].x 				  * (BOARD_WIDTH > seeds[i].x))
+			);
+
+			seeds[i].y = (
+				((BOARD_HEIGHT - (seeds[i].y - BOARD_HEIGHT)) * (BOARD_HEIGHT <= seeds[i].y)) +
+				(seeds[i].y 				  * (BOARD_HEIGHT > seeds[i].y))
+			);
+		}
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	// // cleanup
+	// // unload our texture so it can be cleaned up
+	// UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
