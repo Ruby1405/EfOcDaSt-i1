045c0e08efecbc7a26290a45baeab647727bbb9b
Ruby <ruby.calmerman@gmail.com>
2025-11-25 15:32:28 +0100

    separated stuff

---
+++ src/definitions.c
@@ -0,0 +1,4 @@
+#define uint16 unsigned short
+#define uint32 unsigned int
+#define sint16 short
+#define sint32 int
---
+++ src/polygon.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+#include "raylib.h"
+
+#include "definitions.c"
+
+const uint16 POLYGON_CAP_INIT = 8;
+typedef struct Polygon
+{
+	uint16 pointsCap;
+	uint16 pointsCount;
+	Vector2 *points;
+} Polygon;
+
+void PolygonInit(Polygon *poly)
+{
+    poly->pointsCap = POLYGON_CAP_INIT;
+    poly->pointsCount = 0;
+    poly->points = (Vector2 *)malloc(sizeof(Vector2) * POLYGON_CAP_INIT);
+    return;
+}
+
+inline void PolygonAddPoint(Polygon *poly, Vector2 point)
+{
+	if (poly->pointsCount >= poly->pointsCap)
+	{
+		poly->pointsCap *= 2;
+		poly->points = (Vector2 *)realloc(poly->points, sizeof(Vector2) * poly->pointsCap);
+	}
+	poly->points[poly->pointsCount++] = point;
+	return;
+}
+
+void PolygonAddPointAt(Polygon *poly, Vector2 point, uint16 index)
+{
+    if (poly->pointsCount >= poly->pointsCap)
+    {
+        poly->pointsCap *= 2;
+        poly->points = (Vector2 *)realloc(poly->points, sizeof(Vector2) * poly->pointsCap);
+    }
+    for (uint16 i = poly->pointsCount; i > index; i--)
+    {
+        poly->points[i] = poly->points[i - 1];
+    }
+    poly->points[index] = point;
+    poly->pointsCount++;
+    return;
+}
+
+void PolygonClear(Polygon *poly)
+{
+    poly->pointsCount = 0;
+    return;
+}
+
+void PolygonFree(Polygon *poly)
+{
+    free(poly->points);
+    poly->points = NULL;
+    poly->pointsCap = 0;
+    poly->pointsCount = 0;
+    return;
+}
