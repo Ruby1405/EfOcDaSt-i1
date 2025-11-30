930c64f173574d4c26498bcd25636241089b252c
Ruby1405 <ruby.calmerman@gmail.com>
2025-11-30 13:52:32 +0100

    updated definitions

--- include/definitions.h
+++ include/definitions.h
@@ -1,4 +1,11 @@
-#define uint16 unsigned short
-#define uint32 unsigned int
-#define sint16 short
-#define sint32 int
+#include <stdint.h>
+
+#define uint8 uint8_t
+#define uint16 uint16_t
+#define uint32 uint32_t
+#define uint64 uint64_t
+
+#define sint8 int8_t
+#define sint16 int16_t
+#define sint32 int32_t
+#define sint64 int64_t
