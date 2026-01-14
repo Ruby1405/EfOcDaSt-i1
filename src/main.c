9c0d3879a8e7c141834d9b261aafec27ded09335
Ruby1405 <ruby.calmerman@gmail.com>
2026-01-14 09:33:35 +0100

    cleaning

--- src/main.c
+++ src/main.c
@@ -1,11 +1,3 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
 
 #include <stdlib.h>     
 #include <stdio.h>
@@ -14,9 +6,6 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "raylib.h"
 #include "raymath.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-
-#include "definitions.h"
 #include "myMath.h"
 
 int main ()
